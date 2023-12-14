f2fdd50ad6957ecfa9e9f75f0bfb0fc80da38d51
caiosantos.dev@outlook.com <seu@email.com>
2023-12-14 10:59:17 -0300

    finalizando projeto

--- front/src/service/api.ts
+++ front/src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://kanva.onrender.com"
+export const BASE_URL = "https://kanva-ua0k.onrender.com"
 
 export const api = axios.create({
     baseURL: BASE_URL
