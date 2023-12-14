981f10aea60c33d8a07a664f8b415f5d3ea0dcca
caiosantos.dev@outlook.com <seu@email.com>
2023-12-14 09:36:18 -0300

    add url deploy

--- front/src/service/api.ts
+++ front/src/service/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL_DEV = "http://localhost:3000"
 export const BASE_URL = "https://kanva.onrender.com"
 
 export const api = axios.create({
-    baseURL: BASE_URL_DEV
+    baseURL: BASE_URL
 })
